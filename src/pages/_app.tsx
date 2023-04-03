14c31592ee7f5be5f2253a72202f9c01754c3437
webproject67 <webproject67@gmail.com>
2023-04-03 13:28:47 +0300

    инициализирует material-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 
+const cache = createCache({
+  key: 'css',
+  prepend: true,
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CacheProvider value={cache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
