95dd55ad85d692f6f8c5ee91593c614d5da72b63
webproject67 <webproject67@gmail.com>
2023-04-03 12:47:00 +0300

    инициализирует classnames и sass

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import '@/styles/globals.css';
+import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
