cfba09037399ffff7e5adef8abf996b68f488005
webproject67 <webproject67@gmail.com>
2023-04-05 09:35:03 +0300

    добавит глобальные стили

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
-        <CssBaseline />
+        <CssBaseline enableColorScheme />
         <Main />
         <NextScript />
       </body>
