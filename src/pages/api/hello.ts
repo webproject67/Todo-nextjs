c7ecaa63ec57e2eb16d51dc29c4c7eee36b04926
webproject67 <webproject67@gmail.com>
2023-04-14 08:35:37 +0300

    установит mongoose

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mongoose from 'mongoose';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await mongoose
+    .connect(`${process.env.DB_URL}${process.env.DB_NAME}`)
+    .then(() => console.log('Connected!'))
+    .catch((err) => console.log(err));
+
   res.status(200).json({ name: 'John Doe' });
 }
